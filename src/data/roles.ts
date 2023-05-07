34a5dc9328b81408f4c306ab038f4687685e283b
shivam mane <shivam@mail.com>
2023-05-07 18:07:49 +0530

    roles

---
+++ src/data/roles.ts
@@ -0,0 +1,11 @@
+const roles = [
+    "Frontend Developer",
+    "Backend Developer",
+    "Angular Developer",
+    "ReactJs Developer",
+    "Express.js Developer",
+    "Python Developer",
+    "Java Developer"
+]
+
+export default roles
